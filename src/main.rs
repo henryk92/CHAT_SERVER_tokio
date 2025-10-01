da913edd56298c7c9bc36db25e914059d5bb5bcf
henryk92 <henryk92@naver.com>
2025-10-01 21:06:38 +0900

    0.03

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
-use futures::SinkExt;
+use futures::{SinkExt, StreamExt};
 use tokio::net::TcpListener;
-use tokio_stream::StreamExt;
 use tokio_util::codec::{Framed, LinesCodec};
 
 #[tokio::main]
-// main을 동기 함수로 만들어 비동기 코드를 작동하는 매크로
 async fn main() -> anyhow::Result<()> {
-    // 반환값을 anyhow::Result<()>을 사용해 오류(?) 처리를 간략화
     let server = TcpListener::bind("127.0.0.1:6684").await?;
-    // 127.0.0.1:6684 소켓 바인드 (Future 반환후 await 으로 대기)
+    // 127.0.0.1:6684 소켓 바인드
+
     loop {
         let (mut tcp, _) = server.accept().await?;
         // accept()는 Future의 Result<(TcpStream, SocketAddr)> 값을 반환
         println!("Connect");
-        let mut lines = Framed::new(tcp, LinesCodec::new());
-        while let Some(line) = lines.next().await {
-            let mut line = line?;
-            println!("{line}");
-            line.push_str(" ♥");
-            lines.send(line).await?;
-        }
+
+        tokio::spawn(async move {
+            let mut lines = Framed::new(tcp, LinesCodec::new());
+            // Framed를 이용하여 저수준 바이트 스트림을 고수준 메시지 단위 스트림으로 변경 (LinesCodec 코딩방식 사용)
+            // LinesCodec
+            // - read: TCP에서 바이트를 읽다가 \n을 만나면 거기서 끊어서 String으로 반환
+            // - write: String을 바이트로 바꾸고, 자동으로 \n을 붙여서 TCP에 씀
+
+            let (mut sink, mut stream) = lines.split::<String>();
+            // lines를 쓰기(Sink) / 읽기(Stream) 로 분리.
+
+            while let Some(line) = stream.next().await {
+                // 비동기적으로 .next().await를 호출하면 String 메시지를 한 줄씩 얻을 수 있음
+                let mut line = match line {
+                    Ok(l) => l,
+                    Err(e) => {
+                        eprintln!("Error reading line: {:?}", e);
+                        break;
+                    }
+                };
+                println!("{line}");
+                line.push_str(" return");
+                // 읽은 메세지 뒤에 문자 붙임
+                if let Err(e) = sink.send(line).await {
+                    eprintln!("Error sending line: {:?}", e);
+                    break;
+                }
+            }
+        });
     }
 }
